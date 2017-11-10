267001bf4de8cc69a66cac45f6d9b28a3bdb7da6
sgnl <rayrfarias@gmail.com>
2017-11-10 00:18:19 -1000

    ✨ adds spacebar pausing feature & updatescopy
    📝  updates docs

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ Open the app you will see:
 Click on it to start the timer:
 
 ![Alt text](docs/pomodoro_ex_002.gif?raw=true "Click to start the timer")
+
+<kbd>Space</kbd> will pause the Timer
+
+*Stop the Timer by clicking on it again*
+
+You may have noticed why its "at-a-glance".
+
+If you're truly patient, you will get able to config (🚧 WIP)
+
+There will be <kbd>Keyboard</kbd> Shortcuts (🚧 PLANNED)
+
+The configuration will be more robust (🚧 PLANNED)
+
+Slack Integration (🚧 PLANNED)
--- ui_src/ui/core.cljs
+++ ui_src/ui/core.cljs
@@ -8,15 +8,17 @@
 (defonce interval-duration (atom 1500))
 (defonce duration (atom 0))
 (defonce timer-is-active (atom false))
+(defonce timer-is-paused (atom false))
+
+(.addEventListener js/document "keyup" (fn [e]
+                                         (when (= (.-code e) "Space")
+                                           (swap! timer-is-paused not))))
 
 (defn- str->int [s]
   #(js/parseInt s))
 
 (defn calculate-percentage [duration interval-duration]
   (.floor js/Math (* (/ duration interval-duration) 100)))
-
-(defn duration-as-percentage []
-  (str (calculate-percentage @duration @interval-duration) "%"))
 
 (defn play-sound [type]
   (cond
@@ -33,7 +35,8 @@
   (swap! interval-process
     #(.setInterval js/window
       (fn []
-        (swap! duration inc)
+        (if (= @timer-is-paused false)
+          (swap! duration inc))
         (when (= @duration interval-duration)
           (stop-timer @interval-process)
           (swap! timer-is-active not)
@@ -51,27 +54,36 @@
 (defn pomodoro []
   [:section.pomodoro.ui
     [:h2.time
-      [:span.minutes (.floor js/Math (/ @duration 60)) "m "]
-      [:span.seconds (mod @duration 60) "s"]
-     ]
-    [:h2.percentage (duration-as-percentage)]])
+      [:span (.floor js/Math (/ @duration 60))]
+      [:span.smaller-text "m "]
+      [:span (mod @duration 60)]
+      [:span.smaller-text "s"]]
+
+    [:h2.percentage
+      [:span (calculate-percentage @duration @interval-duration)]
+      [:span.smaller-text "%"]]])
+
+(defn footer []
+  [:footer
+    [:div.icn-cog]])
 
 (defn root-component []
   [:div.root
-    {:on-click (fn [^js/Event e]
+    {:on-click (fn [e]
                  (.preventDefault e)
                  (if (= @timer-is-active false)
                    (do (start-timer @interval-duration)
+                       (reset! timer-is-paused false)
                        (swap! timer-is-active not))
                    (do (stop-timer @interval-process)
-                       (swap! timer-is-active not))))
-    }
+                       (swap! timer-is-active not))))}
     ; [:canvas#circle-progress
     ;  {:width 220
     ;   :height 220
     ;   :data-progress-amount (calculate-percentage @duration @interval-duration)}]
+    [visor]
     [pomodoro]
-    [visor]])
+    [footer]])
 
 (reagent/render
   [root-component]
